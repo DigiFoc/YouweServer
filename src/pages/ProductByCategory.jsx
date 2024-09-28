76efdb12681193e567db0c7fc283ac8ac270622b
DigiFoc <123333848+DigiFoc@users.noreply.github.com>
2024-09-28 21:59:37 +0530

    Update ProductByCategory.jsx

--- src/pages/ProductByCategory.jsx
+++ src/pages/ProductByCategory.jsx
@@ -1,27 +1,28 @@
-import React, { useContext, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Context } from '../Utils/Context';
-import FlaseDeal from '../components/FlaseDealCard/FlaseDeal';
+// import React, { useContext, useEffect } from 'react'
+// import { Link, useParams } from 'react-router-dom'
+// import { Context } from '../Utils/Context';
+// import FlaseDeal from '../components/FlaseDealCard/FlaseDeal';
 
 const ProductByCategory = () => {
-    const { str } = useParams();
+    // const { str } = useParams();
 
-    const { getCategoryData, setCategoryData } = useContext(Context)
+    // const { getCategoryData, setCategoryData } = useContext(Context)
 
-    const getCategory = async () => {
-        const res = await fetch(`http://localhost:8000/api/productsbycategory/${str}`);
-        const resData = await res?.json();
-        setCategoryData(resData);
-    }
+    // const getCategory = async () => {
+    //     const res = await fetch(`http://localhost:8000/api/productsbycategory/${str}`);
+    //     const resData = await res?.json();
+    //     setCategoryData(resData);
+    // }
 
 
-    useEffect(() => {
-        getCategory();
-    }, [getCategoryData])
+    // useEffect(() => {
+    //     getCategory();
+    // }, [getCategoryData])
 
     return (
         <>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 md:w-[85%] mx-auto">
+                {/*
                 {Array?.isArray(getCategoryData) ? (
                     getCategoryData?.map((item) => (
                         <Link to={`/product/${item?.id}`} key={item?.id} className='hover:scale-105 w-fit h-fit md:my-4'>
@@ -32,6 +33,7 @@ const ProductByCategory = () => {
                         loading...
                     </div>
                 )}
+                */}
             </div>
         </>
     )
